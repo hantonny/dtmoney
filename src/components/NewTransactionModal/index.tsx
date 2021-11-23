5371f836f0cc6a5b1403de29484220fd67b851ee
Hantonny Korrea <hantonny.20@gmail.com>
2021-11-23 17:42:12 -0400

    :sparkles: - Inserindo transação na API

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,9 +1,11 @@
 import { FormEvent, useState } from 'react'
 import Modal from 'react-modal'
+import { api } from '../../services/api'
 import { Container, TransactionTypeContainer, RadioBox } from './styles'
 import closeImg from '../../assets/close.svg'
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
+
 
         
 interface NewTransactionModalProps {
@@ -19,12 +21,14 @@ export function NewTransactionModal({isOpen,onRequestClose } : NewTransactionMod
 
     function handleCreateNewTransaction(event: FormEvent) {
         event.preventDefault()
-        console.log({
+        const data = {
             title,
             value,
             category,
             type
-        })
+        }
+
+        api.post('/transactions', data)
     }
 
     return (
